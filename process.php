7be4e612dae6f743c7f44622946e3faef0c29286
fjordski <fordheacock@gmail.com>
2016-04-13 15:21:42 -0500

    Reformat Process.php output
    Displays contact info on separate lines.

--- process.php
+++ process.php
@@ -2,9 +2,12 @@
 $name = $_POST["name"];
 $email = $_POST["email"];
 $message = $_POST["message"];
-	
-echo $name;
-echo $email;
-echo $message;
-	
+
+echo "<pre>";
+$email_body = "";
+$email_body .= "Name " . $name . "\n";
+$email_body .=  "E-mail " . $email . "\n";
+$email_body .=  "Message " . $message . "\n";
+echo $email_body;
+echo "</pre>"
 ?>
