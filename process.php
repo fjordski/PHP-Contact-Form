4fca2688108a48a4327452d20a505df829e40abd
fjordski <fordheacock@gmail.com>
2016-04-13 15:35:49 -0500

    Updates
    Updates

--- process.php
+++ process.php
@@ -9,5 +9,6 @@ $email_body .= "Name " . $name . "\n";
 $email_body .=  "E-mail " . $email . "\n";
 $email_body .=  "Message " . $message . "\n";
 echo $email_body;
-echo "</pre>"
+echo "</pre>";
+header("Location: thank_you_page.php");
 ?>
---
+++ thank_you_page.php
@@ -0,0 +1,6 @@
+<?php
+<div>
+	<h1>Thank You!</h1>
+	<p>Thanks for the email! I&rsquo;ll contact you shortly.</p>
+	</div>
+?>
